c0dc3efcc970adb258b8b408fa9db2de7e243635
Natasha S <vrthngcnts@gmail.com>
2020-12-07 23:18:33 +0300

    scratch cleared, styled components, axios, react router + dom, redux added

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { ConnectedTask as Task } from './Task/Task'
-export { ConnectedControl as Control } from './Control/Control'
-export { ConnectedList as List } from './List/List'
--- src/core/App.js
+++ src/core/App.js
@@ -1,18 +1,6 @@
 import React from 'react';
 
-import { Control, List } from '../components';
-
-import './App.css';
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Control />
-        <List />
-      </div>
-    );
-  }
+export const App = () => {
+  return <></>
 }
 
-export { App }
